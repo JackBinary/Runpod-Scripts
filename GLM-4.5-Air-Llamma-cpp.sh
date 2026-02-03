48a73052fc40a69c44db40d8a32c4266e0f344f0
JackBinary <JMG.EXE@gmail.com>
2026-02-03 11:18:51 -0600

    Modify GLM-4.5-Air-Llamma-cpp.sh for model download
    Updated model download method and paths in the script.

--- GLM-4.5-Air-Llamma-cpp.sh
+++ GLM-4.5-Air-Llamma-cpp.sh
@@ -12,16 +12,18 @@ cd llama.cpp
 HIPCXX="$(hipconfig -l)/clang" HIP_PATH="$(hipconfig -R)" cmake -S . -B build -DGGML_HIP=ON -DAMDGPU_TARGETS=gfx942 -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release -- -j $(nproc)
 
-# Install dependencies
-pip install huggingface_hub hf_transfer
-
-# Download model
-hf download unsloth/GLM-4.5-Air-GGUF \
-  --include "UD-Q8_K_XL/GLM-4.5-Air-UD-Q8_K_XL-*.gguf"
+# Download Model
+mkdir -p /workspace/GLM-4.5-Air-GGUF
+curl -L -o /workspace/GLM-4.5-Air-GGUF/GLM-4.5-Air-UD-Q8_K_XL-00001-of-00003.gguf \
+  "https://huggingface.co/unsloth/GLM-4.5-Air-GGUF/resolve/main/UD-Q8_K_XL/GLM-4.5-Air-UD-Q8_K_XL-00001-of-00003.gguf"
+curl -L -o /workspace/GLM-4.5-Air-GGUF/GLM-4.5-Air-UD-Q8_K_XL-00002-of-00003.gguf \
+  "https://huggingface.co/unsloth/GLM-4.5-Air-GGUF/resolve/main/UD-Q8_K_XL/GLM-4.5-Air-UD-Q8_K_XL-00002-of-00003.gguf"
+curl -L -o /workspace/GLM-4.5-Air-GGUF/GLM-4.5-Air-UD-Q8_K_XL-00003-of-00003.gguf \
+  "https://huggingface.co/unsloth/GLM-4.5-Air-GGUF/resolve/main/UD-Q8_K_XL/GLM-4.5-Air-UD-Q8_K_XL-00003-of-00003.gguf"
 
 # run the model
 /llama.cpp/build/bin/llama-server \
-  -m /workspace/GLM-4.7-GGUF/UD-Q8_K_XL/GLM-4.5-Air-UD-Q8_K_XL-00001-of-00003.gguf \
+  -m /workspace/GLM-4.5-Air-GGUF/GLM-4.5-Air-UD-Q8_K_XL-00001-of-00003.gguf \
   --host 0.0.0.0 \
   --port 8080 \
   -ngl 999 \
